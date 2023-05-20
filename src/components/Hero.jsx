208d674244052740c9251f140df1b30aab1ce398
SamYusuf101 <samyusuf101@gomail.com>
2023-05-20 03:17:49 -0700

    creating hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { Computers } from "./canvas";
 
 const Hero = () => {
   return (
-    <div className='text-red-400 pt-20'>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0
+      top-[120px] max-w-8xl mx-auto flex flex-row items-start
+      gap-5`}
+      >
+        <div className="flex justify-center items-center flex-col mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#395BBF]" />
+          <div className="w-1 sm:h-80 h-40 blue-gradient" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,91 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
-import {styles} from '../styles'
-import {navLinks } from '../constants'
-import {logo, menu, close } from '../assets'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center 
-    fixed top-0 py-3 z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7xl
-      mx-auto'>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center 
+    fixed top-0 py-3 z-20 bg-primary`}
+    >
+      <div
+        className="w-full flex justify-between items-center max-w-9xl
+      mx-auto"
+      >
         <Link
-        to="/"
-        className='flex items-center gap-2'
-        onClick={() => {setActive("")
-      window.scrollTo(0,0)
-    }}
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
-          <img src={logo} alt="logo" className='w-13 h-13
-        object-contain' />
-        <p className='text-white text-[18px] font-bold cursor-pointer'>Sam {""}
-        <span className=''>| Portfolio</span></p>
-
+          <img
+            src={logo}
+            alt="logo"
+            className="w-13 h-13 
+        object-contain"
+          />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Sam &nbsp;
+            <span className="sm:block hidden">| Portfolio</span>
+          </p>
         </Link>
-        
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } 
+            hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px]
+              object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${!toggle ? "hidden" : "flex"} p-6 black-gradient
+          absolute top-20 right-0 mx-4 my-2 min-w-[140px]
+          z-10 rounded-xl`}
+          >
+            <ul
+              className="list-none flex flex-col justify-end
+            items-start gap-4"
+            >
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } 
+            font-poppins text-[16px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
-  )
-
-  }
-export default Navbar
+  );
+};
+export default Navbar;
