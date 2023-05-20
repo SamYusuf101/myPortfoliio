4c5f8a3d035360906c16e44e1603f6c6ccbad1f0
SamYusuf101 <samyusuf101@gomail.com>
2023-05-20 14:54:50 -0700

    updating about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,6 @@ const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="pt-10 xs:w-[250px] w-full ">
       <motion.div
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px]
       rounded-[20px] shadow-card"
       >
@@ -39,13 +38,12 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary
       text-[17px] max-w-3xl leading-[30px] "
       >
