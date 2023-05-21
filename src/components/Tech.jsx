6e664aeb49d2ce04a73b259592ec21ced4ba640e
SamYusuf101 <samyusuf101@gomail.com>
2023-05-21 04:37:44 -0700

    created skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,24 +7,15 @@ import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 3.5 }}
-      >
-        <h2 className={styles.sectionHeadText}>Skills</h2>
-      </motion.div>
-      <div
-        className="flex flex-row flex-wrap mt-10
+    <div
+      className="flex flex-row flex-wrap mt-10
   justify-center gap-10"
-      >
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div>
+    >
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
   );
 };
