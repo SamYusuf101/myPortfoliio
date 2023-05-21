74d3b9a91f9dc82d5557f6b5ffa9e9b6d72b828b
SamYusuf101 <samyusuf101@gomail.com>
2023-05-21 04:08:33 -0700

    updated skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,22 +1,32 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-
+import { motion } from "framer-motion";
 import React from "react";
+import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div
-      className="flex flex-row flex-wrap
+    <div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 3.5 }}
+      >
+        <h2 className={styles.sectionHeadText}>Skills</h2>
+      </motion.div>
+      <div
+        className="flex flex-row flex-wrap mt-10
   justify-center gap-10"
-    >
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      >
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default SectionWrapper(Tech, "tech");
+export default SectionWrapper(Tech, "skills");
