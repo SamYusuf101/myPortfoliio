c5f97ae2876180f997cef30295079929758acfbb
SamYusuf101 <samyusuf101@gomail.com>
2023-05-22 04:04:57 -0700

    creating contact

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,8 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./planet/scene.gltf");
+  return <primitive object={earth.scene} />;
 };
 
 const EarthCanvas = () => {
@@ -17,10 +18,16 @@ const EarthCanvas = () => {
       camera={{}}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls />
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
       </Suspense>
     </Canvas>
   );
 };
 
-export default Earth;
+export default EarthCanvas;
