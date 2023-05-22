5dd648c0936dca72bae020f66b7a63f69de1ca59
SamYusuf101 <samyusuf101@gomail.com>
2023-05-22 03:07:28 -0700

    created works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -91,7 +91,6 @@ const Works = () => {
       </motion.div>
       <div className="w-full flex">
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px]
 max-w-3xl leading-[30px]"
         >
