da50b67e43f74ea7945709b23a5a70f8b3527b83
SamYusuf101 <samyusuf101@gomail.com>
2023-05-22 04:11:13 -0700

    creating contact

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,7 +6,9 @@ import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
-  return <primitive object={earth.scene} />;
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
 };
 
 const EarthCanvas = () => {
@@ -15,7 +17,12 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
